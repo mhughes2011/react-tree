9a9d816df8692299ce61f38c69f3f58f7df067e3
M Hughes <mhughes2011@gmail.com>
2021-07-08 15:57:04 -0700

    refactored again to JSX Expressions

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const title = <h1>My First React Element!</h1>;
+const desc = 'I just learned how to create a React node and render it to the DOM.';
+const myTitleID = 'main-title';
+const name = 'Max';
 
-const desc = <p>I just learned how to create a React node and render it to the DOM.</p>;
-
-const header = React.createElement(
-    'header',
-    {},
-    title,
-    desc
+const header = (
+    <header>
+    {/* This is a JSX comment.  Must use this format */}
+        <h1 id={myTitleID}>{name}'s First React Element</h1>
+        <p>{desc}</p>
+    </header>
 );
 
 ReactDOM.render(
