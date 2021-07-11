55a174fd27b552a05305fab824a4639b05640700
M Hughes <mhughes2011@gmail.com>
2021-07-10 20:47:09 -0700

    created a Counter and Player component

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header() {
+const Header = () => {
     return (
         <header>
             <h1>Scoreboard</h1>
@@ -7,7 +7,29 @@ function Header() {
     );
 }
 
+const Counter = () => {
+    return (
+        <div className='counter'>
+            <button className='counter-action decrement'> - </button>
+            <span className='counter-score'>35</span>
+            <button className='counter-action increment'> + </button>
+        </div>
+    );
+}
+
+const Player = () => {
+    return (
+        <div className='player'>
+            <span className='player-name'>
+                Max
+            </span>
+
+            <Counter />
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <Header />,
+    <Player />,
     document.getElementById('root')
 );
