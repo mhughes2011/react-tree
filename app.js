9b5607cfb51c9d466572f07e0f02e0b544671629
M Hughes <mhughes2011@gmail.com>
2021-07-12 17:52:27 -0700

    added player keys

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const players = [
     {
         name: "Guil",
-        score: 50
+        score: 50,
+        id: 1
       },
       {
         name: "Treasure",
-        score: 85
+        score: 85,
+        id: 2
       },
       {
         name: "Ashley",
-        score: 95
+        score: 95,
+        id: 3
       },
       {
         name: "James",
-        score: 80
+        score: 80,
+        id: 4
       }
 ];
 
@@ -61,7 +65,8 @@ const App = (props) => {
             {props.initialPlayers.map( player => 
                 <Player 
                     name= {player.name}
-                    score={player.score}
+                    score= {player.score}
+                    key= {player.id.toString()}
                 />
             )}
         </div>
